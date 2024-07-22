fe44aaaf1f8e4fbf07215058660623411105eea8
crafty-alex <97107234+crafty-alex@users.noreply.github.com>
2024-07-22 10:29:19 +0200

    Update Application.java

--- api/src/main/java/application/Application.java
+++ api/src/main/java/application/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RepositoryRestMvcAutoConfiguration.class}) // disables auto-created REST endpoints (not secure!)
 @EnableCaching
 public class Application /*extends SpringBootServletInitializer*/ {
 
